61b19553e16dcf8bbf2e8656d64074b992516d19
Akhil9249 <akhklcv430@gmail.com>
2024-04-25 20:32:53 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Counter from './components/Counter'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+// import './App.css'
+import Counter from "./components/Counter";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+    const [show, setShow] = useState(true);
 
-  const onIncrement= ()=>{
-    if(count<10){
-      setCount((prev)=>prev+1 )
-    }
-  }
+    const onIncrement = () => {
+        if (count < 10) {
+            setCount((prev) => prev + 1);
+        }
+        setShow(true);
+    };
 
-    const onDecrement= ()=>{
+    const onDecrement = () => {
+        if (count > 0) {
+            setCount((prev) => prev - 1);
+        }
+        setShow(false);
+    };
 
-      if(count>0){
-        setCount((prev)=>prev-1 )
-      }
-  }
-
-  return (
-    <>
-      <h1>Count Show</h1>
-      <Counter onIncrement={onIncrement} onDecrement={onDecrement} count={count}/>
-    </>
-  )
+    return (
+        <>
+            <Counter onIncrement={onIncrement} onDecrement={onDecrement} count={count} show={show} />
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,54 @@
-import React from 'react'
+import React from "react";
+import "./Counter.css";
 
-function Counter({onIncrement,onDecrement,count}) {
-  return (
-    <div>
-      <h1>Count is {count}</h1>
-      <button onClick={onIncrement} >Increment</button>
-      <button onClick={onDecrement} >Decrement</button>
-    </div>
-  )
+function Counter({ onIncrement, onDecrement, count, show }) {
+    return (
+        <div className="Counter-main-container">
+            <div className="Counter-main-left">
+                <div className="Counter-main-left-top">
+                    <h1 onClick={onDecrement}>-</h1>
+                    <div className="Counter-main-left-top-center">
+                        <h1>{count}</h1>
+                    </div>
+                    <h1 onClick={onIncrement}>+</h1>
+                </div>
+                <div className="Counter-main-left-bottom">
+                    <div
+                        className="Counter-main-left-bottom-left"
+                        style={show ? { background: "rgb(204, 179, 228)" } : { background: "rgb(240, 235, 245)" }}
+                    >
+                        <h1
+                            onClick={onDecrement}
+                            style={show ? { color: "rgb(250, 248, 248)" } : { color: "rgb(93, 93, 182)" }}
+                        >
+                            -
+                        </h1>
+                    </div>
+                    <div
+                        className="Counter-main-left-bottom-right"
+                        style={show ? { background: "rgb(240, 235, 245)" } : { background: "rgb(204, 179, 228)" }}
+                    >
+                        <h1
+                            onClick={onIncrement}
+                            style={show ? { color: "rgb(93, 93, 182)" } : { color: " rgb(250, 248, 248)" }}
+                        >
+                            +
+                        </h1>
+                    </div>
+                </div>
+            </div>
+            <div className="Counter-main-right">
+                <div className="Counter-main-right-top">
+                    <h1>INCREENT</h1>
+                    <h1>DECREENT</h1>
+                </div>
+                <div className="Counter-main-right-bottom">
+                    <h1>SHOWABLE</h1>
+                    <h1>BUTTON</h1>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Counter
+export default Counter;
